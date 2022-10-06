6c4cb9a6011e66223957aecd3e5c3ed902edf06a
Zitoune83 <olive83190@outlook.fr>
2022-10-06 13:36:59 +0200

    Fct createClientPipe

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
 
-int main(int argc, char* argv[])
-{
-    printf("Hello world !\n");
+#define NAME_PIPE_CLIENT 8
 
-    return 0;
+
+int createClientPipe();
+//	create a named pipe for communicate with the server and return its file descriptor.
+//	Name of the pipe is PID.
+
+int main (int argc, char *agv[]){
+
+	printf("CREAT = %d\n",createClientPipe());
+
+
+
+
+	return 0;
+}
+
+
+
+int createClientPipe(){
+
+	char clientPipe[NAME_PIPE_CLIENT];
+	
+	sprintf(clientPipe,"%d",getpid());
+
+       if(mkfifo(clientPipe, 0666)== -1){
+       
+       	return -1;
+       }
+
+       return open(clientPipe,O_RDWR);
 }
